643c442a072a092d849755c3ec48f3a7feff06ab
Roland <matleroland@gmail.com>
2024-06-13 18:43:13 +0200

    Kalendár öszes nap müködik

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 const $container = document.querySelector(".js-container")
 
 function templateCard(day) {
-    const card = document.createElement("div")
-    const front = document.createElement("div")
-    const back = document.createElement("div")
+    const card = document.createElement("div");
+    const front = document.createElement("div");
+    const back = document.createElement("div");
 
-    card.classList = "card"
-    front.classList = "card_content card_front"
-    back.classList = "card_content card_back"
+    card.classList = "card";
+    front.classList = "card_content card_front";
+    back.classList = "card_content card_back is-flipped";
 
-    front.innerHTML = `<h2>December ${day}</h2>`
-    back.innerHTML = `<p>Már csak ${24 - day} nap van Karácsonyig</p>`
+    front.innerHTML = `<h2>December ${day}</h2>`;
 
-    card.appendChild(front)
-    card.appendChild(back)
+if (day === 24) {
+    back.innerHTML = `<p>Ma van karácsony napja</p>`;
+}else{
+    back.innerHTML = `<p>Már csak ${24 - day} nap van Karácsonyig</p>`;
+}
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener("click", () => {
+        front.classList.toggle("is-flipped");
+        back.classList.toggle("is-flipped");
+    });
 
     return card
 }
@@ -22,7 +32,7 @@ function render() {
 
     for (let i = 1; i <= 24; i++) {
 
-        const newCard = templateCard(i)
+        const newCard = templateCard(i);
 
         $container.appendChild(newCard);
     }
