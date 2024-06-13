3caead1931ea4195fa31a3952b72b9375c319575
Roland <matleroland@gmail.com>
2024-06-13 18:05:48 +0200

    js-ben kártyák helyetesítése

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const $container = document.querySelector(".js-container")
+
+const render = () => {
+
+    let html = ""
+
+    for (let i = 1; i <= 24; i++) {
+        const template = `
+        <div class="card">
+            <div class="card_content card_front">December ${i}.</div>
+            <div class="card_content card_back">Már csak ${24 - i} nap van Karácsonyig</div>
+        </div>
+        `
+
+        html += template
+    }
+
+    $container.innerHTML = html
+
+}
+
+render();
