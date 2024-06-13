3d36e6ad9d05edb5f7164c4b23f68de74eacfe0b
Roland <matleroland@gmail.com>
2024-06-13 18:21:43 +0200

    js rész újra gondolása

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
 const $container = document.querySelector(".js-container")
 
-const render = () => {
+function templateCard(day) {
+    const card = document.createElement("div")
+    const front = document.createElement("div")
+    const back = document.createElement("div")
 
-    let html = ""
+    card.classList = "card"
+    front.classList = "card_content card_front"
+    back.classList = "card_content card_back"
+
+    front.innerHTML = `<h2>December ${day}</h2>`
+    back.innerHTML = `<p>Már csak ${24 - day} nap van Karácsonyig</p>`
+
+    card.appendChild(front)
+    card.appendChild(back)
+
+    return card
+}
+
+function render() {
 
     for (let i = 1; i <= 24; i++) {
-        const template = `
-        <div class="card">
-            <div class="card_content card_front">December ${i}.</div>
-            <div class="card_content card_back">Már csak ${24 - i} nap van Karácsonyig</div>
-        </div>
-        `
 
-        html += template
+        const newCard = templateCard(i)
+
+        $container.appendChild(newCard);
     }
 
-    $container.innerHTML = html
-
-}
+};
 
 render();
